e67daaede116ea09d782affa25a24d9002b4a9e6
Brandyli <46945617+Brandyli@users.noreply.github.com>
2020-05-25 00:57:15 -0400

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,1 @@
+SELECT * FROM (SELECT A.name, A.hour, A.timestamp, B.max_high FROM (SELECT name, high, timestamp, SUBSTRING(timestamp, 12, 2) AS hour  FROM "04" db) A INNER JOIN (SELECT name, SUBSTRING(timestamp, 12, 2) AS hour, MAX(high) AS max_high FROM "04" GROUP BY name, SUBSTRING(timestamp, 12, 2)) B ON A.name = B.name AND A.hour = B.hour AND A.high = B.max_high) ORDER BY name, hour
